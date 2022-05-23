4bdff2ef073447cdab4d470211816d486dfd59a3
unknown <shaghayeghdostmohamadii@gmail.com>
2022-05-23 16:46:34 +0430

    add image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import img from './img.png';
 import './styles/App.css';
 
 const App = () => {
   return (
     <div className='container'>
-      <Header name='shaghayegh' address='tehran' pelak={3} date={1401} />
+      {/* <Header name='shaghayegh' address='tehran' pelak={3} date={1401} /> */}
+      <div>
+        <img src={img} />
+      </div>
+
     </div>
   )
 };
